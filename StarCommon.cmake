71f9a5a146b4769df5f7af80e78c40480966492a
Dmitri Smirnov <d.s@plexoos.com>
2016-02-08 18:05:06 -0500

    StarCommon: Use the same -m flag as in ROOT CXX flags

--- StarCommon.cmake
+++ StarCommon.cmake
@@ -9,3 +9,16 @@ elseif(COMPILER_SUPPORTS_CXX0X)
 else()
 	message(STATUS "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
 endif()
+
+
+# Since most of STAR projects depend on ROOT check the flags and use the same
+if(ROOT_FOUND)
+
+	string(REGEX MATCH "(^|[\t ]+)-m([\t ]*)(32|64)([\t ]+|$)" STAR_ROOT_CXX_FLAGS_M ${ROOT_CXX_FLAGS})
+
+	if (STAR_ROOT_CXX_FLAGS_M)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m${CMAKE_MATCH_3}")
+		message(STATUS "Found -m${CMAKE_MATCH_3} option in $ROOT_CXX_FLAGS (root-config). Will add it to $CMAKE_CXX_FLAGS")
+	endif()
+
+endif()
