star-bnl/star-prop

commit e9e005173a1839296441c2e4a2982b86f6e8b3a0

allow to define path to star source code

StarCommon.cmake
@@ -259,8 +259,8 @@ endfunction()
 function(STAR_ADD_LIBRARY star_lib_dir)
 
 	# First check that the path exists
-	if( NOT IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${star_lib_dir} )
-		message( WARNING "StarCommon: Subdirectory \"${star_lib_dir}\" not found in ${CMAKE_CURRENT_SOURCE_DIR}. Skipping" )
+	if( NOT IS_DIRECTORY ${star_lib_dir} )
+		message( WARNING "StarCommon: Directory \"${star_lib_dir}\" not found. Skipping" )
 		return()
 	endif()
 

CMakeLists.txt
@@ -2,8 +2,10 @@ cmake_minimum_required(VERSION 3.2 FATAL_ERROR)
 
 project(star-cvs)
 
+set(STAR_SRC "$ENV{STAR}" CACHE PATH "Path to star software source code")
+
 # Add to path in order to pick up the FindXXX.cmake files included in this project
-list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/star-cmake")
+list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}")
 
 # STAR software depends on ROOT
 find_package(ROOT COMPONENTS Table HistPainter Minuit Geom Spectrum)
@@ -36,11 +38,11 @@ include_directories(
 	${LIBXML2_INCLUDE_DIR}
 	${LOG4CXX_INCLUDE_DIR}
 	${MYSQL_INCLUDE_DIRS}
-	${CMAKE_CURRENT_SOURCE_DIR}
-	${CMAKE_CURRENT_SOURCE_DIR}/StRoot
-	${CMAKE_CURRENT_SOURCE_DIR}/StRoot/RTS/include
-	${CMAKE_CURRENT_SOURCE_DIR}/StRoot/RTS/src
-	${CMAKE_CURRENT_SOURCE_DIR}/StRoot/RTS/trg/include
+	${STAR_SRC}
+	${STAR_SRC}/StRoot
+	${STAR_SRC}/StRoot/RTS/include
+	${STAR_SRC}/StRoot/RTS/src
+	${STAR_SRC}/StRoot/RTS/trg/include
 	${CMAKE_BINARY_DIR}/${STAR_ADDITIONAL_INSTALL_PREFIX}/include
 	# The following directories need to be included until header files can
 	# be generated from idl
@@ -265,5 +267,5 @@ set( star_lib_dirs
 
 
 foreach( star_lib_dir ${star_lib_dirs} )
-	star_add_library( ${star_lib_dir} )
+	star_add_library( "${STAR_SRC}/${star_lib_dir}" )
 endforeach()

commit f5ed52c3beac71637298a7b5445615c3a946b9f1

fixup
This actually seems to make it crash earlier, but it is a correct
change.

Fixes: e9e00517 ('allow to define path to star source code')

StarCommon.cmake
@@ -274,8 +274,8 @@ function(STAR_ADD_LIBRARY star_lib_dir)
 	# Deal with headers
 
 	# Search for default LinkDef if not specified
-	file( GLOB user_linkdefs "${CMAKE_CURRENT_SOURCE_DIR}/${star_lib_dir}/*LinkDef.h"
-	                         "${CMAKE_CURRENT_SOURCE_DIR}/${star_lib_dir}/*LinkDef.hh" )
+	file( GLOB user_linkdefs "${star_lib_dir}/*LinkDef.h"
+	                         "${star_lib_dir}/*LinkDef.hh" )
 
 	if( NOT ARG_LINKDEF AND user_linkdefs )
 		# Get the first LinkDef from the list