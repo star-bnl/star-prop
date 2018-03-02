star-bnl/star-prop

commit 467b7c0eb8ad7398c6b18c6bda83446004e68828

defer Linkdef.h generation to the build phase
This allows for parallelization of the process for faster builds.
There was also a bug in the original code where unwanted pragma's were
added. For example in StRoot/StSvtDbMaker_LinkDef.h one would get

    #pragma link C++ class StSvtDbMaker;

coming from the human-generated Linkdef as well as automatic

    #pragma link C++ class StSvtDbMaker+;

StarCommon.cmake
@@ -94,126 +94,29 @@ endfunction()
 
 
 #
-# Generates a basic LinkDef header ${CMAKE_CURRENT_BINARY_DIR}/${star_lib_dir}_LinkDef.h
-# by parsing the user provided header files with standard linux utilities awk and sed.
+# Sets up generation of an automatic LinkDef header ${CMAKE_CURRENT_BINARY_DIR}/${star_lib_dir}_LinkDef.h
 #
 function(STAR_GENERATE_LINKDEF star_lib_dir dict_headers)
+	cmake_parse_arguments(ARG "" "" "LINKDEF;LINKDEF_HEADERS" ${ARGN})
 
 	# Set default name for LinkDef file
 	set( linkdef_file "${CMAKE_CURRENT_BINARY_DIR}/${star_lib_dir}_LinkDef.h" )
-	set_source_files_properties(${linkdef_file} PROPERTIES GENERATED TRUE)
-
-	message(STATUS "StarCommon: Generating LinkDef header: ${linkdef_file}")
-
-	# Check availability of required system tools
-	find_program(EXEC_AWK NAMES gawk awk)
-	find_program(EXEC_SED NAMES gsed sed)
-
-	if(NOT EXEC_AWK OR NOT EXEC_SED)
-		message(FATAL_ERROR "StarCommon: FATAL: STAR_GENERATE_LINKDEF function requires awk and sed commands")
-	endif()
-
-	cmake_parse_arguments(ARG "" "" "LINKDEF;LINKDEF_HEADERS" ${ARGN})
-
-	# If provided by the user read the contents of their LinkDef file into a list
-	set( linkdef_contents )
-
+	set( gen_linkdef_args "-o;${linkdef_file};${ARG_LINKDEF_HEADERS}" )
 	if( ARG_LINKDEF )
-		file(READ ${ARG_LINKDEF} linkdef_contents)
-		# Convert file lines into a CMake list
-		STRING(REGEX REPLACE ";" "\\\\;" linkdef_contents "${linkdef_contents}")
-		STRING(REGEX REPLACE "\n" ";" linkdef_contents "${linkdef_contents}")
+		list( APPEND gen_linkdef_args "-l;${ARG_LINKDEF}" )
 	endif()
 
-	# Parse header files for ClassDef() statements and collect all entities into
-	# a list
-	set( dict_entities )
-	set( dict_valid_headers )
+	add_custom_command(
+		OUTPUT ${linkdef_file}
+		COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/gen_linkdef.sh" ${gen_linkdef_args}
+		DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/gen_linkdef.sh"
+		)
 
-	foreach( header ${ARG_LINKDEF_HEADERS} )
-
-		# Parse for special namespace marked with $NMSPC in the header file
-		set( my_exec_cmd ${EXEC_AWK} "match($0,\"namespace[[:space:]]+(\\\\w+).*\\\\$NMSPC\", a) { printf(a[1]\"\\r\") }" )
-		execute_process(COMMAND ${my_exec_cmd} ${header} OUTPUT_VARIABLE extracted_namespace OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-		set( my_exec_cmd ${EXEC_AWK} "match($0,\"^[[:space:]]*ClassDef[[:space:]]*\\\\(([^#]+),.*\\\\)\",a){ printf(a[1]\"\\r\") }" )
-
-		execute_process( COMMAND ${my_exec_cmd} ${header} COMMAND ${EXEC_SED} -e "s/\\s\\+/;/g"
-			OUTPUT_VARIABLE extracted_dict_objects OUTPUT_STRIP_TRAILING_WHITESPACE )
-
-		# Prepend dictionary types with namespace
-		if( extracted_namespace )
-			string(REGEX REPLACE "([^;]+)" "${extracted_namespace}::\\1" extracted_dict_objects "${extracted_dict_objects}")
-		endif()
-
-		list( APPEND dict_entities ${extracted_dict_objects} )
-
-		if( extracted_dict_objects )
-			list( APPEND dict_valid_headers ${header} )
-		else()
-			# if header base name matches linkdef_contents then use this header
-			get_filename_component( header_base_name ${header} NAME_WE )
-
-			if( "${linkdef_contents}" MATCHES "${header_base_name}" )
-				list( APPEND dict_valid_headers ${header} )
-			endif()
-		endif()
-
-	endforeach()
-
-
-	# Special case dealing with StEvent containers
-	set( dict_entities_stcontainers )
-
-	if( "${star_lib_dir}" MATCHES "StEvent" )
-		foreach( header ${ARG_LINKDEF_HEADERS} )
-			set( my_exec_cmd ${EXEC_AWK} "match($0,\"^[[:space:]]*StCollectionDef[[:space:]]*\\\\(([^#]+)\\\\)\",a){ printf(a[1]\"\\r\") }" )
-			
-			execute_process( COMMAND ${my_exec_cmd} ${header} COMMAND ${EXEC_SED} -e "s/\\s\\+/;/g"
-				OUTPUT_VARIABLE extracted_dict_objects OUTPUT_STRIP_TRAILING_WHITESPACE )
-
-			list( APPEND dict_entities_stcontainers ${extracted_dict_objects} )
-
-			if( extracted_dict_objects )
-				list( APPEND dict_valid_headers ${header} )
-			endif()
-		endforeach()
-	endif()
-
-	# Assign new validated headers 
-	set( ${dict_headers} ${dict_valid_headers} PARENT_SCOPE )
-
-	# Write contents to the generated *_LinkDef.h file
-
-	if( "${linkdef_contents}" MATCHES "pragma[ \t]+link[ \t]+off[ \t]+all" )
-		file(WRITE ${linkdef_file} "#ifdef __CINT__\n\n")
-	else()
-		file(WRITE ${linkdef_file} "#ifdef __CINT__\n\n#pragma link off all globals;\n#pragma link off all classes;\n#pragma link off all functions;\n\n")
-	endif()
-
-	foreach( linkdef_line ${linkdef_contents} )
-		if( "${linkdef_line}" MATCHES "#pragma[ \t]+link")
-			file( APPEND ${linkdef_file} "${linkdef_line}\n" )
-		endif()
-	endforeach()
-	
-	file( APPEND ${linkdef_file} "\n\n// Collected dictionary entities\n\n" )
-
-	foreach( dict_entity ${dict_entities} )
-		if( NOT "${linkdef_contents}" MATCHES "class[ ]+${dict_entity}[< ;+-]+")
-			file( APPEND ${linkdef_file} "#pragma link C++ class ${dict_entity}+;\n" )
-		endif()
-	endforeach()
-
-	file( APPEND ${linkdef_file} "\n\n" )
-
-	foreach( dict_entity ${dict_entities_stcontainers} )
-		file( APPEND ${linkdef_file} "#pragma link C++ class StPtrVec${dict_entity}-;\n" )
-		file( APPEND ${linkdef_file} "#pragma link C++ class StSPtrVec${dict_entity}-;\n" )
-	endforeach()
-
-	file( APPEND ${linkdef_file} "#endif\n" )
-
+	# XXX We used to return useful headers to ${dict_headers}. Since the
+	# scanning is defered to the build time we can't do it here anymore.
+	# One possible solution would be to merge gen_linkdef.sh and rootcint
+	# stages together. For now let's just return all available headers.
+	set( ${dict_headers} ${ARG_LINKDEF_HEADERS} PARENT_SCOPE )
 endfunction()
 
 

gen_linkdef.sh
@@ -1,0 +1,97 @@
+#!/bin/bash
+
+set -e
+
+function print_usage() {
+    echo "Usage: $0 [-l input_Linkdef.h] -o output [inputs...]" 1>&2;
+    exit 1;
+}
+
+eval set -- "$(getopt -o "o:l:" -n "$0" -- "$@")"
+
+while true; do
+	case "$1" in
+		-l)
+			input_linkdef_path="$2" ;
+			shift 2 ;;
+		-o)
+			output_linkdef_path="$2" ;
+			shift 2 ;;
+		--)
+			shift ;
+			break ;;
+		*)
+			echo "Unknown option '$1'" ;
+			print_usage ;;
+	esac
+done
+
+if [ -z "${output_linkdef_path}" ]; then
+	echo "Missing -o"
+	print_usage
+fi
+
+classes=()
+stcontainer_classes=()
+headers=() # TODO this is filled, but not used yet
+
+for INFILE in "$@"; do
+	echo "Scanning $INFILE" >&2
+	file_classes=( $(awk 'match($0,"^[[:space:]]*ClassDef[[:space:]]*\\(([^#]+),.*\\)", a) { print a[1] }' "$INFILE") )
+	namespace="$(awk 'match($0,"namespace[[:space:]]+(\\w+).*\\$NMSPC", a) { print a[1] }' "$INFILE")"
+	if [ -n "$namespace" ]; then
+		for i in "${!file_classes[@]}"
+		do
+			file_classes[i]=$namespace"::"${file_classes[$i]}
+		done
+	fi
+	classes+=( "${file_classes[@]}" )
+
+	if [ "${#file_classes[@]}" -ne 0 ]; then
+		# if there is at least one ClassDef, then use the header file
+		headers+=( "$INFILE" ) # TODO maybe just print to stdout?
+	else
+		# take filename without extension as the class name
+		header_filename="$(basename "$INFILE")"
+		header_classname="${header_filename%.*}"
+
+		# if classname is in the original linkdef, then use the header file
+		if [ -n "$input_linkdef_path" ] && grep "$header_classname" "$input_linkdef_path" 1>/dev/null; then
+			headers+=( "$INFILE" )
+		fi
+	fi
+
+	stcontainer_classes+=( $(awk 'match($0,"^[[:space:]]*StCollectionDef[[:space:]]*\\(([^#]+)\\)",a) { print a[1] }' "$INFILE") )
+done
+
+# check if array is not empty
+echo "#ifdef __CINT__" > "$output_linkdef_path"
+echo >> "$output_linkdef_path"
+# add "link of all" lines if they are not in the input linkdef
+if [ -z "$input_linkdef_path" ] || ! grep -E "pragma[ \t]+link[ \t]+off[ \t]+all" "$input_linkdef_path" 1>/dev/null; then
+	echo "#pragma link off all globals;" >> "$output_linkdef_path"
+	echo "#pragma link off all classes;" >> "$output_linkdef_path"
+	echo "#pragma link off all functions;" >> "$output_linkdef_path"
+	echo >> "$output_linkdef_path"
+fi
+if [ -n "$input_linkdef_path" ]; then
+	# reuse lines from input linkdef that match the pattern
+	grep -E "#pragma[ \t]+link" "$input_linkdef_path" >> "$output_linkdef_path" || true
+fi
+echo >> "$output_linkdef_path"
+echo >> "$output_linkdef_path"
+echo "// Collected dictionary entities" >> "$output_linkdef_path"
+echo >> "$output_linkdef_path"
+for class in "${classes[@]}"; do
+	# do not duplicate lines from the input linkdef if they were already specified
+	if [ -z "$input_linkdef_path" ] || ! grep -E "class[ ]+$class[< ;+-]+" "$input_linkdef_path" 1>/dev/null; then
+		echo "#pragma link C++ class $class+;" >> "$output_linkdef_path"
+	fi
+done
+echo >> "$output_linkdef_path"
+echo >> "$output_linkdef_path"
+for class in "${stcontainer_classes[@]}"; do
+	echo "#pragma link C++ class StPtrVec${class}-;" >> "$output_linkdef_path"
+	echo "#pragma link C++ class StSPtrVec${class}-;" >> "$output_linkdef_path"
+done
+echo "#endif" >> "$output_linkdef_path"

commit 92faa5b8f5b92c0a8a268477906e356e1b8faf34

BUGFIX: switch invocation of gen_linkdef.sh to execute_process()
This allows to retrive the list of "good" headers. We need that list
because if we just use all headers, then some of them contain things
that rootcint can't handle.

Fixes: 467b7c0eb ('defer Linkdef.h generation to the build phase')

StarCommon.cmake
@@ -98,7 +98,7 @@ endfunction()
 
 
 #
-# Sets up generation of an automatic LinkDef header ${CMAKE_CURRENT_BINARY_DIR}/${star_lib_dir}_LinkDef.h
+# Generate an automatic LinkDef header ${CMAKE_CURRENT_BINARY_DIR}/${star_lib_dir}_LinkDef.h
 #
 function(STAR_GENERATE_LINKDEF star_lib_dir dict_headers)
 	cmake_parse_arguments(ARG "" "" "LINKDEF;LINKDEF_HEADERS" ${ARGN})
@@ -110,17 +110,18 @@ function(STAR_GENERATE_LINKDEF star_lib_dir dict_headers)
 		list( APPEND gen_linkdef_args "-l;${ARG_LINKDEF}" )
 	endif()
 
-	add_custom_command(
-		OUTPUT ${linkdef_file}
+	# It would be better if we had add_custom_command() here, because that
+	# would allow to scan things in parallel. But we can't do that just
+	# here because we need to retrieve a list of "good" headers to use with
+	# rootcint. The TODO is to merge those two steps and have them use
+	# add_custom_command() so that this can run during make phase.
+	set_source_files_properties(${linkdef_file} PROPERTIES GENERATED TRUE)
+	execute_process(
 		COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/gen_linkdef.sh" ${gen_linkdef_args}
-		DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/gen_linkdef.sh"
+		OUTPUT_VARIABLE used_headers
 		)
 
-	# XXX We used to return useful headers to ${dict_headers}. Since the
-	# scanning is defered to the build time we can't do it here anymore.
-	# One possible solution would be to merge gen_linkdef.sh and rootcint
-	# stages together. For now let's just return all available headers.
-	set( ${dict_headers} ${ARG_LINKDEF_HEADERS} PARENT_SCOPE )
+	set( ${dict_headers} ${used_headers} PARENT_SCOPE )
 endfunction()
 
 

gen_linkdef.sh
@@ -33,7 +33,6 @@ fi
 
 classes=()
 stcontainer_classes=()
-headers=() # TODO this is filled, but not used yet
 
 for INFILE in "$@"; do
 	echo "Scanning $INFILE" >&2
@@ -49,7 +48,7 @@ for INFILE in "$@"; do
 
 	if [ "${#file_classes[@]}" -ne 0 ]; then
 		# if there is at least one ClassDef, then use the header file
-		headers+=( "$INFILE" ) # TODO maybe just print to stdout?
+		echo -n "$INFILE;"
 	else
 		# take filename without extension as the class name
 		header_filename="$(basename "$INFILE")"
@@ -57,7 +56,7 @@ for INFILE in "$@"; do
 
 		# if classname is in the original linkdef, then use the header file
 		if [ -n "$input_linkdef_path" ] && grep "$header_classname" "$input_linkdef_path" 1>/dev/null; then
-			headers+=( "$INFILE" )
+			echo -n "$INFILE;"
 		fi
 	fi
 