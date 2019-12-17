2b49c95ac712137520571069fd1998c6840b6249
Dmitri Smirnov <d.s@plexoos.com>
2019-12-17 17:13:54 -0500

    Rename and modify google drive download script
    The content of the dowloaded file is streamed to stdout to match the behavior of
    curl

--- scripts/gdrive.py
+++ scripts/gdrive.py
@@ -1,20 +1,12 @@
 import requests
 
-def download_file_from_google_drive(id, destination):
+def download_file_from_google_drive(id):
     def get_confirm_token(response):
         for key, value in response.cookies.items():
             if key.startswith('download_warning'):
                 return value
 
         return None
-
-    def save_response_content(response, destination):
-        CHUNK_SIZE = 32768
-
-        with open(destination, "wb") as f:
-            for chunk in response.iter_content(CHUNK_SIZE):
-                if chunk: # filter out keep-alive new chunks
-                    f.write(chunk)
 
     URL = "https://docs.google.com/uc?export=download"
 
@@ -27,16 +19,14 @@ def download_file_from_google_drive(id, destination):
         params = { 'id' : id, 'confirm' : token }
         response = session.get(URL, params = params, stream = True)
 
-    save_response_content(response, destination)    
+    sys.stdout.buffer.write(response.content)
 
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) is not 3:
-        print("Usage: python google_drive.py drive_file_id destination_file_path")
+    if len(sys.argv) is not 2:
+        print("Usage: python gdrive.py drive_file_id")
     else:
         # TAKE ID FROM SHAREABLE LINK
         file_id = sys.argv[1]
-        # DESTINATION FILE ON YOUR DISK
-        destination = sys.argv[2]
-        download_file_from_google_drive(file_id, destination)
+        download_file_from_google_drive(file_id)
