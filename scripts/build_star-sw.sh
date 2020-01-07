d3490455e08089792218c369ff63a5dcaf92023f
Dmitri Smirnov <d.s@plexoos.com>
2020-01-07 12:01:55 -0500

    [docker] Rename s/_TAG/_REF/ + other minor updates

--- scripts/build_star-sw.sh
+++ scripts/build_star-sw.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # Set default values for primary parameters
-: ${STAR_CVS_TAG:="master"}
+: ${STAR_CVS_REF:="master"}
 : ${STAR_BUILD_TYPE:="Release"}
 : ${STAR_BASE_IMAGE:="centos7"}
 : ${STAR_SW_DIR:="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"}
-: ${STAR_SW_TAG:="master"}
+: ${STAR_SW_REF:="master"}
 : ${DOCKER_ID_NAMESPACE:="starbnl/"}
 
 
 function print_usage() {
 	echo "Usage: $0 [<star-cvs_branch_or_tag [=master]> ]" \
-	              " [-t Debug|Release|RelWithDebInfo]" \
+	              " [-t Debug|Release|RelWithDebInfo [=Release]]" \
 	              " [-b <star_base_image [=centos7]>]" \
 	              " [-p <path_to_star-sw [=$STAR_SW_DIR]>]" \
 	              " [-s <star-sw_branch_or_tag [=master]>]" 1>&2;
@@ -43,7 +43,7 @@ while true; do
 			STAR_SW_DIR="$2" ;
 			shift 2 ;;
 		-s)
-			STAR_SW_TAG="$2" ;
+			STAR_SW_REF="$2" ;
 			shift 2 ;;
 		--)
 			shift ;
@@ -54,23 +54,23 @@ done
 # Process the only positional argument
 if [ -n "$1" ]
 then
-	STAR_CVS_TAG=$1
+	STAR_CVS_REF=$1
 fi
 
 # Just print out the variable's values for the record
 echo "The following variables are set:"
-echo -e "\t STAR_CVS_TAG:          \"$STAR_CVS_TAG\""
+echo -e "\t STAR_CVS_REF:          \"$STAR_CVS_REF\""
 echo -e "\t STAR_BUILD_TYPE:       \"$STAR_BUILD_TYPE\""
 echo -e "\t STAR_BASE_IMAGE:       \"$STAR_BASE_IMAGE\""
 echo -e "\t STAR_SW_DIR:           \"$STAR_SW_DIR\""
-echo -e "\t STAR_SW_TAG:           \"$STAR_SW_TAG\""
+echo -e "\t STAR_SW_REF:           \"$STAR_SW_REF\""
 echo -e "\t DOCKER_ID_NAMESPACE:   \"$DOCKER_ID_NAMESPACE\""
 
-# Format image name as star-sw:$STAR_CVS_TAG-$STAR_BUILD_TYPE-sw-$STAR_SW_TAG
-STAR_IMAGE_TAG=$([ "$STAR_CVS_TAG" = "master" ] && echo "latest" || echo "$STAR_CVS_TAG")
+# Format image name as star-sw:$STAR_CVS_REF-$STAR_BUILD_TYPE-sw-$STAR_SW_REF
+STAR_IMAGE_TAG=$([ "$STAR_CVS_REF" = "master" ] && echo "latest" || echo "$STAR_CVS_REF")
 STAR_IMAGE_TAG+=$([ "$STAR_BUILD_TYPE" = "Release" ] && echo "" || echo "-$STAR_BUILD_TYPE")
 STAR_IMAGE_TAG+=$([ "$STAR_BASE_IMAGE" = "centos7" ] && echo "" || echo "-$STAR_BASE_IMAGE")
-STAR_IMAGE_TAG+=$([ "$STAR_SW_TAG"  = "master" ] && echo "" || echo "-sw-$STAR_SW_TAG")
+STAR_IMAGE_TAG+=$([ "$STAR_SW_REF"  = "master" ] && echo "" || echo "-sw-$STAR_SW_REF")
 
 
 cmd="docker build --rm -t ${DOCKER_ID_NAMESPACE}star-base-${STAR_BASE_IMAGE} \
@@ -83,10 +83,10 @@ $cmd
 
 cmd="docker build --rm -t ${DOCKER_ID_NAMESPACE}star-sw-build:${STAR_IMAGE_TAG} \
     -f ${STAR_SW_DIR}/docker/Dockerfile.star-sw \
-    --build-arg STAR_CVS_TAG=${STAR_CVS_TAG} \
+    --build-arg STAR_CVS_REF=${STAR_CVS_REF} \
     --build-arg STAR_BUILD_TYPE=${STAR_BUILD_TYPE} \
     --build-arg STAR_BASE_IMAGE=${DOCKER_ID_NAMESPACE}star-base-${STAR_BASE_IMAGE} \
-    --build-arg STAR_SW_TAG=${STAR_SW_TAG} \
+    --build-arg STAR_SW_REF=${STAR_SW_REF} \
     --target build-stage \
     ${STAR_SW_DIR}
 "
@@ -96,10 +96,10 @@ $cmd
 
 cmd="docker build --rm -t ${DOCKER_ID_NAMESPACE}star-sw:${STAR_IMAGE_TAG} \
     -f ${STAR_SW_DIR}/docker/Dockerfile.star-sw \
-    --build-arg STAR_CVS_TAG=${STAR_CVS_TAG} \
+    --build-arg STAR_CVS_REF=${STAR_CVS_REF} \
     --build-arg STAR_BUILD_TYPE=${STAR_BUILD_TYPE} \
     --build-arg STAR_BASE_IMAGE=${DOCKER_ID_NAMESPACE}star-base-${STAR_BASE_IMAGE} \
-    --build-arg STAR_SW_TAG=${STAR_SW_TAG} \
+    --build-arg STAR_SW_REF=${STAR_SW_REF} \
     ${STAR_SW_DIR}
 "
 echo
@@ -108,7 +108,7 @@ $cmd
 
 cmd="docker build --rm -t ${DOCKER_ID_NAMESPACE}star-sw-cons:${STAR_IMAGE_TAG} \
     -f ${STAR_SW_DIR}/docker/Dockerfile.star-sw-cons \
-    --build-arg STAR_CVS_TAG=${STAR_CVS_TAG} \
+    --build-arg STAR_CVS_REF=${STAR_CVS_REF} \
     --build-arg STAR_BUILD_TYPE=${STAR_BUILD_TYPE} \
     --build-arg STAR_BASE_IMAGE=${DOCKER_ID_NAMESPACE}star-base-${STAR_BASE_IMAGE} \
     ${STAR_SW_DIR}
